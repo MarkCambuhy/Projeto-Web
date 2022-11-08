04b0cea7996ddd5c407cbc5e0aebef936284bb6e
Mark <86503313+MarkCambuhy@users.noreply.github.com>
2022-11-07 21:49:57 -0300

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const email = document.querySelector('.email');
 const pass = document.querySelector('.pass');
 const atualiza_input = document.querySelector('.procurar_api');
 const imagem = document.querySelector('.imagem');
-const name_agent= document.querySelector('.name_agent');
+const name_agent = document.querySelector('.name_agent');
 const description = document.querySelector('.description');
 const email_err = document.querySelector('.email_err');
 const pass_err = document.querySelector('.pass_err');
@@ -32,7 +32,7 @@ signin.addEventListener('click', () => {
         "email": email.value,
         "password": pass.value
     };
-    if(verifica_email(email.value) && verifica_senha(pass.value)){
+    if (verifica_email(email.value) && verifica_senha(pass.value)) {
         xhr.open('POST', 'https://reqres.in/api/login');
         xhr.setRequestHeader('Content-Type', 'application/json');
         xhr.addEventListener('load', function () {
@@ -52,34 +52,44 @@ signin.addEventListener('click', () => {
 
 function verifica_inputSearch() {
     let aux = localStorage.getItem('logado');
-    if(aux != null){
+    if (aux != null) {
         atualiza_input.style.display = 'inline';
     }
 };
 
-atualiza_input.addEventListener('keypress', function () {
-    let agente_esc = document.querySelector('.procurar_api').value;
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', 'https://valorant-api.com/v1/agents', true);
-    xhr.addEventListener('load', function () {
-        if (xhr.status === 200 && xhr.readyState === 4) {
-            const res = JSON.parse(xhr.responseText);
-            console.log(res);
-            for(i=0; i<=20; i++){
-                let agente = res.data[i].displayName;
-                if(agente.toLowerCase() === agente_esc.toLowerCase()){
-                   imagem.src = res.data[i].fullPortrait;
-                   name_agent.innerHTML = res.data[i].displayName;
-                   description.innerHTML = res.data[i].description;
-                   agentes_info.style.display = 'flex';
-                    break;
-                } 
+atualiza_input.addEventListener('keypress', function (event) {
+    if(event.key === 'Enter'){
+        let contador = 0;
+        let agente_esc = document.querySelector('.procurar_api').value;
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET', 'https://valorant-api.com/v1/agents', true);
+        xhr.addEventListener('load', function () {
+            if (xhr.status === 200 && xhr.readyState === 4) {
+                const res = JSON.parse(xhr.responseText);
+                console.log(res);
+                for (i = 0; i <= 20; i++) {
+                    let agente = res.data[i].displayName;
+                    if (agente.toLowerCase() === agente_esc.toLowerCase()) {
+                        imagem.src = res.data[i].fullPortrait;
+                        name_agent.innerHTML = res.data[i].displayName;
+                        description.innerHTML = res.data[i].description;
+                        agentes_info.style.display = 'flex';
+                        break;
+                    }
+                    contador++;
+                }
+                if (contador > 20) {
+                    imagem.src = 'https://cdn-icons-png.flaticon.com/512/56/56748.png';
+                    name_agent.innerHTML = 'Invalido';
+                    description.innerHTML = '';
+                    agentes_info.style.display = 'flex';
+                }
+            } else {
+                console.error('Bad request!');
             }
-        } else {
-            console.error('Bad request!');
-        }
-    });
-    xhr.send();
+        });
+        xhr.send();
+    }
 });
 
 function verifica_email(email) {
